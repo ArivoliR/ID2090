f73f28777cb4c2efb91e3029f4ea59219e055e7c
Arivoli Ramamoorthy <53344634+ArivoliR@users.noreply.github.com>
2024-03-07 17:38:22 +0530

    Update question_2.sh

--- assignment_1/question_2/question_2.sh
+++ assignment_1/question_2/question_2.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/bash
+<<COMMENT
+Name: Arivoli Ramamoorthy
+Roll number: EE23B008
+Date: 02-03-2024
+Description: 
+-Find the n-th term in a generalized recurrence relation.
+-Recurrence Relation: af[n] = bf[n - 1] + cf[n - 2].  
+Input: Coefficients a, b, c, and initial values f[1] and f[2] are provided via a file.  
+Output: Computes and outputs the n-th term for each test case. 
+COMMENT
 
 #Check if there are 2 command line arguments
 if [ "$#" -ne 2 ]; then
@@ -39,7 +49,7 @@ calculate_nth_term() {
 read -r a b c f1 f2 <<<"$(cat "$1" | tr -d ',')"
 #echo "Coefficients: a=$a, b=$b, c=$c, f1=$f1, f2=$f2"
 if [[ -z $a || -z $b || -z $c || -z $f1 || -z $f2 ]]; then
-	echo "Error: All coefficients (a, b, c, f1, f2) must be provided in initial.txt."
+	echo "Error: All coefficients (a, b, c, f1, f2) must be provided in $1."
 	exit 1
 fi
 
@@ -49,7 +59,9 @@ IFS= read -r num_testcases <"$2"
 display_correct_usage() {
 	echo "Correct usage: <number of test cases> <test case 1> <test case 2> ..."
 }
+
 actual_num_testcases=$(tail -n +2 "$2" | wc -l)
+
 if [[ $num_testcases -ne $actual_num_testcases ]]; then
 	echo "Error: Number of test cases provided ($actual_num_testcases) does not match the specified number ($num_testcases)."
 	display_correct_usage
@@ -62,3 +74,4 @@ for ((i = 0; i < actual_num_testcases; i++)); do
 	#echo "For n=$n, nth term: $result"
 	echo "$result"
 done < <(tail -n +2 "$2")
+
